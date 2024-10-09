c6661a5324bd3bb1f80a48e9044ffc0daa7c099c
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-09 13:26:15 -0500

    Create products.php

---
+++ products.php
@@ -0,0 +1,9 @@
+<?php 
+require_once("util-db.php");
+require_once("model-products.php");
+$pageTitle = "Products";
+include "view-header.php";
+$products = selectProducts();
+include "view-products.php";
+include "view-footer.php";
+?>
