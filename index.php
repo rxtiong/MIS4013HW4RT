e3d12824da8ea303cda12f957fdcdb7be5c6a80a
rxtiong <122956904+rxtiong@users.noreply.github.com>
2024-10-09 13:06:48 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+$pageTitle = "Home";
+include "view-header.php";
+?>
+    <h1>Homework 3</h1>
+    <p>This homework shows data from a database. Customers have one order and employees have one order, but an order 
+    can have multiple customers and/or employees. Orders contain one product, but products can be in multiple orders.</p>
+    
+<?php 
+include "view-footer.php";
+?>
